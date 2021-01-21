cc782b7b06b633f1999259b245effd46c78c7c89
Tito Harditya W <tito.w@students.amikom.ac.id>
2021-01-21 19:44:34 +0700

    sudah bisa tampil gambar

--- index.php
+++ index.php
@@ -130,18 +130,16 @@
         <?php
         $kolom = 4;
         $i = 1;
-        $upload_dir = '../img/product/';
+        $upload_dir = 'img/product/';
         $sqlProduk = "SELECT * FROM tabel_produk ORDER BY id DESC LIMIT 8";
         $rslt = mysqli_query($kon, $sqlProduk);
         while ($data = mysqli_fetch_array($rslt)) {
-  
-          echo $upload_dir . $data["image"];
+
           if (($i) % $kolom == 1) {
             echo '<tr>';
           }
-         echo '<td><img src=' . $upload_dir . $data["image"], ' class="responsive-img materialboxed", width="200px"/><p class="center" style="font-weight: bold">' . $data['name'] . '</p></td>';
-         echo file_get_contents($upload_dir . $data["image"]); 
-         if (($i) % $kolom == 0) {
+          echo '<td><img src=' . $upload_dir . $data["image"], ' class="responsive-img materialboxed", width="200px"/><p class="center" style="font-weight: bold">' . $data['name'] . '</p></td>';
+          if (($i) % $kolom == 0) {
             echo '</tr>';
           }
           $i++;
